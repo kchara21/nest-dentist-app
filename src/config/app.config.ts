503768701164764f7b528301be4e3b453b4c3399
kchara21 <kalebdavidchara@hotmail.com>
2022-09-25 23:06:21 -0500

    Creating configs to resources

---
+++ src/config/app.config.ts
@@ -0,0 +1,35 @@
+import { registerAs } from '@nestjs/config';
+import { isBoolean } from 'class-validator';
+
+interface HttpConfig {
+  host: string;
+  port: number;
+}
+
+export interface AppConfig {
+  name: string;
+  globalPrefix: string;
+  env: string;
+  timeout: number;
+  language: string;
+  http: HttpConfig;
+  timezone: string;
+  saveLogs: boolean;
+}
+
+export default registerAs(
+  'app',
+  (): AppConfig => ({
+    name: process.env.APP_NAME || 'dentist-api',
+    globalPrefix: process.env.APP_GLOBAL_PREFIX || 'dentist-api/v1',
+    env: process.env.NODE_ENV || 'development',
+    timeout: parseInt(process.env.APP_TIMEOUT_IN_MS) || 180_000,
+    language: process.env.APP_LANGUAGE || 'es',
+    http: {
+      host: process.env.HOST || process.env.APP_HOST || '0.0.0.0',
+      port: parseInt(process.env.APP_PORT) || 3000,
+    },
+    timezone: process.env.APP_TZ || 'America/Guayaquil',
+    saveLogs: process.env.APP_SAVE_LOGS === 'true',
+  }),
+);
---
+++ src/config/auth.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+
+export interface AuthConfig {
+  jwt: {
+    secretKey: string;
+    expirationTime: string;
+  };
+}
+
+export default registerAs(
+  'auth',
+  (): AuthConfig => ({
+    jwt: {
+      secretKey: process.env.JWT_SECRET_KEY,
+      expirationTime: process.env.JWT_EXPIRATION_TIME || '60s',
+    },
+  }),
+);
