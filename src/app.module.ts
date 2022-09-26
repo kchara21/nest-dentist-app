653156063f60c805d3f0b44187639c056f2e203f
kchara21 <kalebdavidchara@hotmail.com>
2022-09-25 23:07:11 -0500

    Updating app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { configService } from './config/config.service';
+import { configDBService } from './config/config.db.service';
 import { UsersModule } from './users/users.module';
 import { AppointmentsModule } from './appointments/appointments.module';
 import { ResultsModule } from './results/results.module';
 import { InvoicesModule } from './invoices/invoices.module';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ConfigModule } from '@nestjs/config';
+import Configs from './config/index';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { RolesGuard } from './auth/guards/roles.guard';
+import { HttpExceptionFilter } from './core/filters/http-exception.filter';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(configDBService.getTypeOrmConfig()),
+    ConfigModule.forRoot({ load: Configs, isGlobal: true }),
     AuthModule,
     UsersModule,
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     AppointmentsModule,
     ResultsModule,
     InvoicesModule,
@@ -23,6 +30,11 @@ import { CategoriesModule } from './categories/categories.module';
     CategoriesModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: JwtAuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+  ],
 })
 export class AppModule {}
